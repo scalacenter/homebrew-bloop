e47cffc4e287727a9d723f7f9b8b5eeae8c5c6a6
Bloopoid <bloop@trashmail.ws>
2018-03-28 13:21:32 +0000

    Updating to Bloop v1.0.0-M8

--- bloop.rb
+++ bloop.rb
@@ -1,9 +1,9 @@
 class Bloop < Formula
   desc "Bloop gives you fast edit/compile/test workflows for Scala."
   homepage "https://github.com/scalacenter/bloop"
-  version "1.0.0-M7"
-  url "https://github.com/scalacenter/bloop/releases/download/v1.0.0-M7/install.py"
-  sha256 "f05f3a643557a2fd321b2aafd3bd66d80a793c8e00b45a042f3a85b1deed1764"
+  version "1.0.0-M8"
+  url "https://github.com/scalacenter/bloop/releases/download/v1.0.0-M8/install.py"
+  sha256 "676dd70909e62b7b14a165bec9c1edbbce2746578b1859ad13e5b87579a39230"
   bottle :unneeded
 
   depends_on "python"
