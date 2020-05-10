92bf22dfb0d0a3dd6fea8ca3ec7a35e60dde700c
Bloopoid <bloop@trashmail.ws>
2020-05-10 23:46:02 +0000

    Updating to Bloop v1.4.0-RC3

--- bloop.rb
+++ bloop.rb
@@ -1,9 +1,9 @@
 class Bloop < Formula
   desc "Installs the Bloop CLI for Bloop, a build server to compile, test and run Scala fast"
   homepage "https://github.com/scalacenter/bloop"
-  version "1.4.0-RC2"
-  url "https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC2/bloop-coursier.json.json"
-  sha256 "f3666855bb57cc4b2008844ce865a7329b9e81b474e038214b25dc997e965796"
+  version "1.4.0-RC3"
+  url "https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC3/bloop-coursier.json.json"
+  sha256 "eae4a227f729bae1b5d31f9ac60dcf699a75a3272a72c4973f9fec5f2b7f5a5b"
   bottle :unneeded
 
   depends_on "bash-completion"
@@ -11,17 +11,17 @@ class Bloop < Formula
   depends_on :java => "1.8+"
 
   resource "bash_completions" do
-    url "https://raw.githubusercontent.com/scalacenter/bloop/v1.4.0-RC2/bash-completions"
+    url "https://raw.githubusercontent.com/scalacenter/bloop/v1.4.0-RC3/bash-completions"
     sha256 "da6b7ecd4109bd0ff98b1c452d9dd9d26eee0d28ff604f6c83fb8d3236a6bdd1"
   end
 
   resource "zsh_completions" do
-    url "https://raw.githubusercontent.com/scalacenter/bloop/v1.4.0-RC2/zsh-completions"
+    url "https://raw.githubusercontent.com/scalacenter/bloop/v1.4.0-RC3/zsh-completions"
     sha256 "58d32c3f005f7791237916d1b5cd3a942115236155a0b7eba8bf36391d06eff7"
   end
 
   resource "fish_completions" do
-    url "https://raw.githubusercontent.com/scalacenter/bloop/v1.4.0-RC2/fish-completions"
+    url "https://raw.githubusercontent.com/scalacenter/bloop/v1.4.0-RC3/fish-completions"
     sha256 "a012a5cc76b57dbce17fad237f3b97bea6946ffc6ea0b61ac2281141038248dd"
   end
 
@@ -30,7 +30,7 @@ class Bloop < Formula
       mkdir "channel"
 
       mv "bloop-coursier.json", "channel/bloop.json"
-      system "coursier", "install", "--install-dir", "bin", "--default-channels=false", "--channel", "channel", "bloop", "-J-Divy.home=/Users/jvicentecantero/.ivy2"
+      system "coursier", "install", "--install-dir", "bin", "--default-channels=false", "--channel", "channel", "bloop", "-J-Divy.home=/home/runner/.ivy2"
 
       resource("bash_completions").stage { bash_completion.install "bash-completions" }
       resource("zsh_completions").stage { zsh_completion.install "zsh-completions" }
