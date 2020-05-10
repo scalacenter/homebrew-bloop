76245dd611915c3129277402de43fc86d4df33d5
Bloopoid <bloop@trashmail.ws>
2020-05-10 23:42:36 +0200

    Updating to Bloop v1.4.0-RC2

--- bloop.rb
+++ bloop.rb
@@ -1,54 +1,43 @@
 class Bloop < Formula
-  desc "Bloop is a build server to compile, test and run Scala fast"
+  desc "Installs the Bloop CLI for Bloop, a build server to compile, test and run Scala fast"
   homepage "https://github.com/scalacenter/bloop"
-  version "1.4.0-RC1"
-  url "https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC1/install.py"
-  sha256 "01cdeb9c26944d73539d47e2ec4bf5f75c19438fa23c06ae83e8aa9479322e4e"
+  version "1.4.0-RC2"
+  url "https://github.com/scalacenter/bloop/releases/download/v1.4.0-RC2/bloop-coursier.json.json"
+  sha256 "f3666855bb57cc4b2008844ce865a7329b9e81b474e038214b25dc997e965796"
   bottle :unneeded
 
   depends_on "bash-completion"
-  depends_on "python3"
+  depends_on "coursier/formulas/coursier"
   depends_on :java => "1.8+"
+
+  resource "bash_completions" do
+    url "https://raw.githubusercontent.com/scalacenter/bloop/v1.4.0-RC2/bash-completions"
+    sha256 "da6b7ecd4109bd0ff98b1c452d9dd9d26eee0d28ff604f6c83fb8d3236a6bdd1"
+  end
+
+  resource "zsh_completions" do
+    url "https://raw.githubusercontent.com/scalacenter/bloop/v1.4.0-RC2/zsh-completions"
+    sha256 "58d32c3f005f7791237916d1b5cd3a942115236155a0b7eba8bf36391d06eff7"
+  end
+
+  resource "fish_completions" do
+    url "https://raw.githubusercontent.com/scalacenter/bloop/v1.4.0-RC2/fish-completions"
+    sha256 "a012a5cc76b57dbce17fad237f3b97bea6946ffc6ea0b61ac2281141038248dd"
+  end
 
   def install
       mkdir "bin"
-      system "python3", "install.py", "--dest", "bin", "--version", version
-      zsh_completion.install "bin/zsh/_bloop"
-      bash_completion.install "bin/bash/bloop"
-      fish_completion.install "bin/fish/bloop.fish"
+      mkdir "channel"
 
-      # We need to create these files manually here, because otherwise launchd
-      # will create them with owner set to `root` (see the plist file below).
-      FileUtils.mkdir_p("log/bloop/")
-      FileUtils.touch("log/bloop/bloop.out.log")
-      FileUtils.touch("log/bloop/bloop.err.log")
+      mv "bloop-coursier.json", "channel/bloop.json"
+      system "coursier", "install", "--install-dir", "bin", "--default-channels=false", "--channel", "channel", "bloop", "-J-Divy.home=/Users/jvicentecantero/.ivy2"
+
+      resource("bash_completions").stage { bash_completion.install "bash-completions" }
+      resource("zsh_completions").stage { zsh_completion.install "zsh-completions" }
+      resource("fish_completions").stage { fish_completion.install "fish-completions" }
 
       prefix.install "bin"
-      prefix.install "log"
   end
-
-  def plist; <<~EOS
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>Label</key>
-    <string>#{plist_name}</string>
-    <key>ProgramArguments</key>
-    <array>
-        <string>#{bin}/bloop</string>
-        <string>server</string>
-    </array>
-    <key>KeepAlive</key>
-    <true/>
-    <key>StandardOutPath</key>
-    <string>#{prefix}/log/bloop/bloop.out.log</string>
-    <key>StandardErrorPath</key>
-    <string>#{prefix}/log/bloop/bloop.err.log</string>
-</dict>
-</plist>
-          EOS
-      end
 
   test do
   end
