75704d3b99b062c434cb3d658c105c7a5cf21ffe
Bloopoid <bloop@trashmail.ws>
2020-05-15 11:27:21 +0000

    Updating to Bloop v1.4.1

--- bloop.rb
+++ bloop.rb
@@ -1,9 +1,9 @@
 class Bloop < Formula
   desc "Installs the Bloop CLI for Bloop, a build server to compile, test and run Scala fast"
   homepage "https://github.com/scalacenter/bloop"
-  version "1.4.0"
-  url "https://github.com/scalacenter/bloop/releases/download/v1.4.0/bloop-coursier.json"
-  sha256 "1bf5f6b82f13fbccf3bfd75da59113529c95cf62b9249ed47e34e18f32dc410d"
+  version "1.4.1"
+  url "https://github.com/scalacenter/bloop/releases/download/v1.4.1/bloop-coursier.json"
+  sha256 "2e6a873183e5e22712913bfdab1331d0a7792167c369fee5be0c83e27572fe12"
   bottle :unneeded
 
   depends_on "bash-completion"
@@ -11,17 +11,17 @@ class Bloop < Formula
   depends_on :java => "1.8+"
 
   resource "bash_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.4.0/bash-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.4.1/bash-completions"
     sha256 "da6b7ecd4109bd0ff98b1c452d9dd9d26eee0d28ff604f6c83fb8d3236a6bdd1"
   end
 
   resource "zsh_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.4.0/zsh-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.4.1/zsh-completions"
     sha256 "58d32c3f005f7791237916d1b5cd3a942115236155a0b7eba8bf36391d06eff7"
   end
 
   resource "fish_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.4.0/fish-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.4.1/fish-completions"
     sha256 "a012a5cc76b57dbce17fad237f3b97bea6946ffc6ea0b61ac2281141038248dd"
   end
 
@@ -36,12 +36,12 @@ class Bloop < Formula
         mv "bash-completions", "bloop"
         bash_completion.install "bloop"
       }
-      
+
       resource("zsh_completions").stage {
         mv "zsh-completions", "_bloop"
         zsh_completion.install "_bloop"
       }
-      
+
       resource("fish_completions").stage {
         mv "fish-completions", "bloop.fish"
         fish_completion.install "bloop.fish"
