7cc7fe7ca120c77986d97ac43c04cb24962866ae
Bloopoid <bloop@trashmail.ws>
2020-12-03 09:59:31 +0100

    Updating to Bloop v1.4.6

--- bloop.rb
+++ bloop.rb
@@ -1,9 +1,9 @@
 class Bloop < Formula
   desc "Installs the Bloop CLI for Bloop, a build server to compile, test and run Scala fast"
   homepage "https://github.com/scalacenter/bloop"
-  version "1.4.5"
-  url "https://github.com/scalacenter/bloop/releases/download/v1.4.5/bloop-coursier.json"
-  sha256 "5eba68b553cc028f1aaf4bf58b444f8e0378c4c32ebf2e34cb6e5961f0f66a28"
+  version "1.4.6"
+  url "https://github.com/scalacenter/bloop/releases/download/v1.4.6/bloop-coursier.json"
+  sha256 "2a62d5465741660228c20080335de3017caaf2dfe52bd45887940ee0b207a8bb"
   bottle :unneeded
 
   depends_on "bash-completion"
@@ -11,17 +11,17 @@ class Bloop < Formula
   depends_on :java => "openjdk@8+"
 
   resource "bash_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.4.5/bash-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.4.6/bash-completions"
     sha256 "da6b7ecd4109bd0ff98b1c452d9dd9d26eee0d28ff604f6c83fb8d3236a6bdd1"
   end
 
   resource "zsh_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.4.5/zsh-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.4.6/zsh-completions"
     sha256 "58d32c3f005f7791237916d1b5cd3a942115236155a0b7eba8bf36391d06eff7"
   end
 
   resource "fish_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.4.5/fish-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.4.6/fish-completions"
     sha256 "a012a5cc76b57dbce17fad237f3b97bea6946ffc6ea0b61ac2281141038248dd"
   end
 
@@ -30,7 +30,7 @@ class Bloop < Formula
       mkdir "channel"
 
       mv "bloop-coursier.json", "channel/bloop.json"
-      system "coursier", "install", "--install-dir", "bin", "--default-channels=false", "--channel", "channel", "bloop", "-J-Divy.home=/home/runner/.ivy2"
+      system "coursier", "install", "--install-dir", "bin", "--default-channels=false", "--channel", "channel", "bloop", "-J-Divy.home=/Users/mduhem/.ivy2"
 
       resource("bash_completions").stage {
         mv "bash-completions", "bloop"
