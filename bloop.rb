04997f8699a830b8c7b50cc7f7e77a64d89a88f1
Bloopoid <bloop@trashmail.ws>
2023-07-05 17:47:37 +0000

    Updating to Bloop v1.5.8

--- bloop.rb
+++ bloop.rb
@@ -1,26 +1,26 @@
 class Bloop < Formula
   desc "Installs the Bloop CLI for Bloop, a build server to compile, test and run Scala fast"
   homepage "https://github.com/scalacenter/bloop"
-  version "1.5.7"
-  url "https://github.com/scalacenter/bloop/releases/download/v1.5.7/bloop-coursier.json"
-  sha256 "836edc551c4f3110440c94b6c88240e084285f4301d8fc61c5b72b4306f6f80e"
+  version "1.5.8"
+  url "https://github.com/scalacenter/bloop/releases/download/v1.5.8/bloop-coursier.json"
+  sha256 "047b3f98bfd47c65ecf23730dfe9e422aedb3c4bd28932424e05a8263ee4106e"
 
   depends_on "bash-completion@2"
   depends_on "coursier/formulas/coursier"
   depends_on "openjdk"
 
   resource "bash_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.5.7/bash-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.5.8/bash-completions"
     sha256 "da6b7ecd4109bd0ff98b1c452d9dd9d26eee0d28ff604f6c83fb8d3236a6bdd1"
   end
 
   resource "zsh_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.5.7/zsh-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.5.8/zsh-completions"
     sha256 "58d32c3f005f7791237916d1b5cd3a942115236155a0b7eba8bf36391d06eff7"
   end
 
   resource "fish_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.5.7/fish-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.5.8/fish-completions"
     sha256 "78511247a88f1d10d5886bfe164dec0af3e0540864d8bd60086e0b9df5cefefe"
   end
 
