e2032f2224c8942c085297aa7921ad4e7f3464f5
Tomasz Godzik <tgodzik@users.noreply.github.com>
2024-01-31 19:35:42 +0100

    Update bloop to v1.5.14

--- bloop.rb
+++ bloop.rb
@@ -1,26 +1,26 @@
 class Bloop < Formula
   desc "Installs the Bloop CLI for Bloop, a build server to compile, test and run Scala fast"
   homepage "https://github.com/scalacenter/bloop"
-  version "1.5.13"
-  url "https://github.com/scalacenter/bloop/releases/download/v1.5.13/bloop-coursier.json"
-  sha256 "d4b9282aeb7a4b770486250cf4dd0c0e30d17a06b908554c81a5bd4a70a78506"
+  version "1.5.14"
+  url "https://github.com/scalacenter/bloop/releases/download/v1.5.14/bloop-coursier.json"
+  sha256 "2520139fa9db2118520a40fba92bca828a2103f982f1604df76e77b7085d9d6f"
 
   depends_on "bash-completion@2"
   depends_on "coursier/formulas/coursier"
   depends_on "openjdk"
 
   resource "bash_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.5.13/bash-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.5.14/bash-completions"
     sha256 "da6b7ecd4109bd0ff98b1c452d9dd9d26eee0d28ff604f6c83fb8d3236a6bdd1"
   end
 
   resource "zsh_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.5.13/zsh-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.5.14/zsh-completions"
     sha256 "58d32c3f005f7791237916d1b5cd3a942115236155a0b7eba8bf36391d06eff7"
   end
 
   resource "fish_completions" do
-    url "https://github.com/scalacenter/bloop/releases/download/v1.5.13/fish-completions"
+    url "https://github.com/scalacenter/bloop/releases/download/v1.5.14/fish-completions"
     sha256 "78511247a88f1d10d5886bfe164dec0af3e0540864d8bd60086e0b9df5cefefe"
   end
 
